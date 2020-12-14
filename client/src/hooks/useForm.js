ab2f999e5226a1812ec911151437a18f095ed535
Clare Jackson <clare-jackson@lambdastudents.com>
2020-12-13 20:57:40 -0500

    Custom hook complete in useForm.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react';
+
+export const useForm = (key, initialValues) => {
+
+  const [values, setValues] = useState(key, initialValues)
+
+  const handleChanges = e => {
+    setValues({
+      ...values, 
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(values)
+  }
+
+  return [values, handleChanges, handleSubmit];
+}
